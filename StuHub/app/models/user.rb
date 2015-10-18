7ab9bc610b4e8c74de6a17c1c28913c0f9168336
liuhaoyu1994 <liuhaoyuca@gmail.com>
2015-10-18 11:13:14 +0000

    Finish log in/log out

--- StuHub/app/models/user.rb
+++ StuHub/app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-    
+    attr_accessor :remember_token
     ##check upper case
     before_save {self.email = email.downcase}
     
@@ -20,4 +20,31 @@ class User < ActiveRecord::Base
     #check length of password
     validates :password, presence: true, length: { minimum: 6 }
 
+    def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+    end
+  
+    ## return a radom password token
+    def User.new_token
+       SecureRandom.urlsafe_base64
+    end
+    
+    # Remember User
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+  # forget user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
+   # if token was paired with digest, return true
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
 end
--- StuHub/config/routes.rb
+++ StuHub/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessins/new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -19,6 +23,10 @@ Rails.application.routes.draw do
   get 'users/new'
   get 'user/new'
   get 'signup' => 'user#new'
+  
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :users
 
   # Example of regular route:
