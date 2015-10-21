13371bd8aa663415367cd6c548ccc3b3d46d0cee
Jonathan Lo <jonathan_lo_4@sfu.ca>
2015-10-20 23:16:05 -0700

    Add a whole bunch of tests and fix others.
    Based on the RoR Tutorial.

--- StuHub/test/controllers/index_controller_test.rb
+++ StuHub/test/controllers/index_controller_test.rb
@@ -1,9 +1,17 @@
 require 'test_helper'
 
 class IndexControllerTest < ActionController::TestCase
-  test "should get index" do
+
+  def setup
+    @user = users(:michael)
+  end
+
+  test "get index page both while logged out or in" do
     get :index
     assert_response :success
+    log_in_as(@user)
+    get :index
+    assert_redirected_to home_path
   end
 
 end
