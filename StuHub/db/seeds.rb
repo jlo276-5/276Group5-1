a8edd51274b39d0be5efbb60d73f89180dfe8a75
Jonathan Lo <jonathan_lo_4@sfu.ca>
2015-10-20 23:29:16 -0700

    Configure the admin db seed.

--- StuHub/db/seeds.rb
+++ StuHub/db/seeds.rb
@@ -6,18 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create!(name:  "Example User",
+User.create!(name:  "Administrator",
              email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar",
-             admin: true)
-
-99.times do |n|
-  name  = Faker::Name.name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password:              password,
-               password_confirmation: password)
-end
+             password: "admin",
+             password_confirmation: "admin",
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
