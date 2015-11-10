3c2c78a8860ce65c779959bb53f13ae31f3a650b
Jonathan Lo <jonathan_lo_4@sfu.ca>
2015-11-09 20:52:50 -0800

    Fixes for the calendar

--- StuHub/test/controllers/events_controller_test.rb
+++ StuHub/test/controllers/events_controller_test.rb
@@ -3,20 +3,25 @@ require 'test_helper'
 class EventsControllerTest < ActionController::TestCase
   setup do
     @event = events(:one)
+    @user = users(:michael)
   end
 
   test "should get index" do
+    log_in_as(@user)
     get :index
     assert_response :success
     assert_not_nil assigns(:events)
   end
 
   test "should get new" do
+    log_in_as(@user)
     get :new
     assert_response :success
   end
 
   test "should create event" do
+    log_in_as(@user)
+
     assert_difference('Event.count') do
       post :create, event: { description: @event.description, end_time: @event.end_time, strat_time: @event.strat_time, title: @event.title }
     end
@@ -25,21 +30,29 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test "should show event" do
+    log_in_as(@user)
+
     get :show, id: @event
     assert_response :success
   end
 
   test "should get edit" do
+    log_in_as(@user)
+
     get :edit, id: @event
     assert_response :success
   end
 
   test "should update event" do
+    log_in_as(@user)
+
     patch :update, id: @event, event: { description: @event.description, end_time: @event.end_time, strat_time: @event.strat_time, title: @event.title }
     assert_redirected_to event_path(assigns(:event))
   end
 
   test "should destroy event" do
+    log_in_as(@user)
+
     assert_difference('Event.count', -1) do
       delete :destroy, id: @event
     end
