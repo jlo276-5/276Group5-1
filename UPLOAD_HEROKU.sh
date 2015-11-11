31eda958ec811d3af06369f12808436f3b4b05dd
Jonathan Lo <jonathan_lo_4@sfu.ca>
2015-11-11 05:06:33 -0800

    Lock for main upload script, used when needs to be kept stable (for marking, etc)

--- UPLOAD_HEROKU.sh
+++ UPLOAD_HEROKU.sh
@@ -1,8 +1,14 @@
 #! /bin/bash
 
 set -e
-old_branch=$(git rev-parse --abbrev-ref HEAD)
-git checkout master
-./pre-push
-git push -f heroku `git subtree split --prefix StuHub master`:master
-git checkout $old_branch
+
+lock="./master_lock"
+if [ -e "$lock" ]; then
+  echo "STUHUB MASTER SITE LOCKED"
+else
+  old_branch=$(git rev-parse --abbrev-ref HEAD)
+	git checkout master
+	./pre-push
+	git push -f heroku `git subtree split --prefix StuHub master`:master
+	git checkout $old_branch
+fi
