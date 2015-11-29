ea77c30ef283488d39d18470ce7f8757468a1fbb
Jonathan Lo <jonathan_lo_4@sfu.ca>
2015-11-28 20:40:15 -0800

    Fix migration error due to removal of paperclip

--- StuHub/db/migrate/20151127025604_create_resources.rb
+++ StuHub/db/migrate/20151127025604_create_resources.rb
@@ -3,7 +3,6 @@ class CreateResources < ActiveRecord::Migration
     create_table :resources do |t|
       t.string :name, null: false
       t.text :description, default: ""
-      t.attachment :resource, null: false
       t.references :group, index: true, foreign_key: true
       t.references :course, index: true, foreign_key: true
       t.references :user, index: true, foreign_key: true
--- StuHub/db/migrate/20151129015542_change_resource_storage_type_in_resource.rb
+++ StuHub/db/migrate/20151129015542_change_resource_storage_type_in_resource.rb
@@ -1,6 +1,5 @@
 class ChangeResourceStorageTypeInResource < ActiveRecord::Migration
   def change
-    remove_attachment :resources, :resource
     add_column :resources, :file_name, :string, null: false
     add_column :resources, :content_type, :string, null: false
   end
