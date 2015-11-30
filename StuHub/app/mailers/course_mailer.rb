f3b62155bcac27447fe7b473ddd8f1e971a904a9
Jonathan Lo <jonathan_lo_4@sfu.ca>
2015-11-30 06:45:03 -0800

    Standardize display of course department and number

--- StuHub/app/mailers/course_mailer.rb
+++ StuHub/app/mailers/course_mailer.rb
@@ -3,13 +3,13 @@ class CourseMailer < ApplicationMailer
   def joined_course(user, course)
     @user = user
     @course = course
-    mail to: user.email, subject: "[StuHub] Joined Course \"#{course.department.name} #{course.number}\" (#{course.department.term.term_name})"
+    mail to: user.email, subject: "[StuHub] Joined Course \"#{course.course_number}\" (#{course.department.term.term_name})"
   end
 
   def dropped_course(user, course)
     @user = user
     @course = course
-    mail to: user.email, subject: "[StuHub] Dropped Course \"#{course.department.name} #{course.number}\" (#{course.department.term.term_name})"
+    mail to: user.email, subject: "[StuHub] Dropped Course \"#{course.course_number}\" (#{course.department.term.term_name})"
   end
 
 end
